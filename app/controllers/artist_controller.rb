b18d00e7d6ea441a7e21c6a258da923bd60cf4c4
Emily Abrash <emabrash@gmail.com>
2016-10-12 11:29:09 -0700

    Added handling for controller delete and create tests

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -3,6 +3,7 @@ class ArtistController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id].to_i)
   end
 
   def new
@@ -12,11 +13,25 @@ class ArtistController < ApplicationController
   end
 
   def create
+    @artist = Artist.new
+    @artist.name = params[:artist][:name]
+    if @artist.save
+      redirect_to artist_path(@artist.id)
+    else
+      render new_artist_path
+    end
   end
 
   def update
   end
 
   def destroy
+    begin
+      @artist = Artist.find(params[:id])
+      if @artist.destroy
+        redirect_to artist_index_path
+      end
+    rescue ActiveRecord::RecordNotFound
+    end
   end
 end
--- test/controllers/artist_controller_test.rb
+++ test/controllers/artist_controller_test.rb
@@ -4,11 +4,13 @@ class ArtistControllerTest < ActionController::TestCase
   test "should get index for artists" do
     get :index
     assert_response :success
+    assert_template :index
   end
 
   test "should get show page for an artist" do
-    get :show, {id: 1 }
+    get :show, {id: artists(:bonjovi).id }
     assert_response :success
+    assert_template :show
   end
 
   test "should get new page for an artist" do
@@ -17,23 +19,41 @@ class ArtistControllerTest < ActionController::TestCase
   end
 
   test "should get edit page for an artist" do
-    get :edit, {id: 1 }
+    get :edit, {id: artists(:bonjovi).id}
     assert_response :success
   end
 
   test "should be able to create an artist" do
-    post :create
-    assert_response :success
+    post :create, { artist: { name: "Taylor Swift" }}
+    assert_response :redirect
+
+    post :create, { artist: { name: nil }}
+    assert_template :new
+  end
+
+  test "Creating an artist changes the number of artists" do
+    assert_difference('Artist.count', 1) do
+      post :create, { artist: { name: "Cat Stevens" }}
+    end
   end
 
   test "should be able to update an artist" do
-    patch :update, {id: 1 }
+    patch :update, {id: artists(:bonjovi).id}
     assert_response :success
   end
 
   test "should be able to delete an artist" do
-    delete :destroy, {id: 1 }
-    assert_response :success
+    delete :destroy, {id: artists(:bonjovi).id}
+    assert_response :redirect
+
+    delete :destroy, {id: -1 }
+    assert_template :destroy
+  end
+
+  test "Deleting an artist changes the number of artists" do
+    assert_difference('Artist.count', -1) do
+      delete :destroy, { id: artists(:bonjovi).id}
+    end
   end
 
 end
